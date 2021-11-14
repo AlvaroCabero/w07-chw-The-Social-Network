403bdb164a2f3df91f3c37a41b9d04c1dbc9c42c
AlvaroCabero <alvarocabero@gmail.com>
2021-11-14 08:20:09 +0100

    Added routes for login and sign up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
-   
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+import Login from "../../components/Login/Login";
+import Signup from "../../components/SignUp/SignUp";
+
+function LoginPage() {
+ 
+  const user = useSelector(({ user }) => user);
+
+
+
+
+  return (
+    
+  );
+}
+
+export default LoginPage;
